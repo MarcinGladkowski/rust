5667344ae31da6211aecc433e1ca762fd4b85275
MarcinGladkowski <marcingladkowski@gmail.com>
2024-05-17 14:22:12 +0200

    exercise: simple calculator from Celsius to Fahrenheit

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -4,22 +4,18 @@ fn main() {
     println!("Hello, world!");
 
     let mut temperature = String::new();
-    let mut metric_scale = String::new();
     println!("Please provide temperature value: ");
     let stdin = io::stdin(); // got stdin
     stdin.read_line(&mut temperature).expect("Error while provide temperature");
 
-    let parsed_metric = metric_scale.trim();
+    // trim_end() removes '\n' from input
+    let unit: char = temperature.trim_end().chars().last().unwrap();
 
-    // validate input
-    println!("Please provide type: Fahrenheit(F) or Celsius (C)");
-    stdin.read_line(&mut metric_scale).expect("Error while provide temperature metric");
+    let value = &temperature[0..temperature.len()-2];
 
-    metric_scale.contains(parsed_metric);
+    let scales: [&char; 2] = [&'F', &'C'];
 
-    let scales: [&str; 2] = ["F", "C"];
-
-    let is_correct_metric: bool = scales.contains(&metric_scale.as_str().trim());
+    let is_correct_metric: bool = scales.contains(&&unit);
 
     if !is_correct_metric {
         println!("Incorrect metric provided. Only {:?} available", scales);
@@ -27,18 +23,16 @@ fn main() {
     }
 
     // validate input
-    println!("You provided {temperature} and metric {metric_scale}");
+    println!("You provided {value}");
 
-    let temperature_value: f64 = temperature.trim().parse().expect("Cannot parse provided temperature");
+    let temperature_value: f64 = value.parse().expect("Cannot parse provided temperature");
 
-    if parsed_metric.eq("F") {
-        // calculate from F to C
+    if unit.eq(&'F') {
         let result: f64 = temperature_value - 32.0 / 1.8;
-
         print!("{}", result);
     }
 
-    if parsed_metric.eq("C") {
+    if unit.eq(&'C') {
         // calculate from C + F
         let result: f64 = temperature_value * 1.8 + 32.0;
 
